3637a1edd9bb532b7b753584ab0ee5e7ed143f56
V3g3tta <j3suadj3@gmail.com>
2022-10-07 15:43:31 -0500

    commit init

--- 19-validacion/procesar.php
+++ 19-validacion/procesar.php
@@ -1,24 +1,10 @@
-<?php
-
-$error = true;
-if(!empty($_POST['nombre']) && !empty($_POST['apellidos']) && !empty($_POST['edad']) &&!empty($_POST['email']) && !empty($_POST['password'])) {
-$error = false;
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellidos'];
-$edad = $_POST['edad'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-}
-?>
-
-
 <!DOCTYPE html>
-<html lang="es">
+<html>
     <head>
         <meta charset="utf-8"/>
-        <title>VALIDACION DE FORMULARIO</title>
+        <title>datos validados correctamente</title>
     </head>
     <body>
         
     </body>
-</html>
+</html>    
